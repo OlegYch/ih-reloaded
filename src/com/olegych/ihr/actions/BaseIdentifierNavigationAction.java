6a126b280db32288ecbe7da9c0a49e424b9fd1f6
OlegYch <OlegYch@.(none)>
2011-09-18 21:07:57 +0300

    refactoring

--- src/com/olegych/ihr/actions/BaseIdentifierNavigationAction.java
+++ src/com/olegych/ihr/actions/BaseIdentifierNavigationAction.java
@@ -29,17 +29,31 @@ class BaseIdentifierNavigationAction extends AnAction {
     final Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
     if (editor != null && project != null) {
       final boolean alwaysClearHighlights = getAlwaysClear();
-      final boolean clearHighlights = HighlightUsagesHandler.isClearHighlights(editor);
+      final boolean clearHighlights = alreadyHighlighted(editor);
       if (alwaysClearHighlights) {
         ((HighlightManagerImpl) HighlightManager.getInstance(project))
             .hideHighlights(editor, HighlightManager.HIDE_BY_ESCAPE | HighlightManager.HIDE_BY_ANY_KEY);
       } else if (clearHighlights) {
-        e.getActionManager().getAction("HighlightUsagesInFile").actionPerformed(e);
+        highlight(e);
       }
-      e.getActionManager().getAction("HighlightUsagesInFile").actionPerformed(e);
-      e.getActionManager().getAction(navigationActionId).actionPerformed(e);
+      highlight(e);
+      if (alreadyHighlighted(editor)) {
+        invokeAction(e, navigationActionId);
+      }
       editor.getSelectionModel().removeSelection();
     }
+  }
+
+  private boolean alreadyHighlighted(Editor editor) {
+    return HighlightUsagesHandler.isClearHighlights(editor);
+  }
+
+  private void highlight(AnActionEvent e) {
+    invokeAction(e, "HighlightUsagesInFile");
+  }
+
+  private void invokeAction(AnActionEvent e, String navigationActionId) {
+    e.getActionManager().getAction(navigationActionId).actionPerformed(e);
   }
 
   private boolean getAlwaysClear() {
